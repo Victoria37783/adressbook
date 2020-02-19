f9467a55a916fa60a95016c5a7c1cf944cf4e230
victoria.be <victoria.be@maxitechs.com>
2020-02-19 10:59:47 +0200

    delete

---
+++ Homeworkadress/Homeworkadress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homeworkadress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
